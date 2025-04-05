486171add2a5892a264ec4db9b71f0b2f4e0befa
Rielly Kirk <Riellykirk13@gmail.com>
2025-04-04 23:23:22 -0400

    first I added transloco

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,14 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideTranslateService } from '@ngx-translate/core';
-
 import { routes } from './app.routes';
 
 // data services to connect to backend API
 import { HabitService } from './services/habit.service';
 import { provideHttpClient } from '@angular/common/http';
+//--Import Transloco -----------------------------------------------------------------------------------------------
+import { TranslocoHttpLoader } from './transloco-loader';
+import { provideTransloco } from '@jsverse/transloco';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), 
@@ -15,6 +17,15 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(),
     provideTranslateService({
       defaultLanguage: 'en'
-    })
+    }), provideHttpClient(), provideTransloco({
+        config: { 
+          availableLangs: ['en', 'es', 'fr'],
+          defaultLang: 'en',
+          // Remove this option if your application doesn't support changing language in runtime.
+          reRenderOnLangChange: true,
+          prodMode: !isDevMode(),
+        },
+        loader: TranslocoHttpLoader
+      })
   ]
 };
